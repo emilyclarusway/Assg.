0a625ff402d264bebbc628fc765b8c5029f0af09
Emily <emily@clarusway.com>
2024-03-10 14:45:27 +0300

    first commit

---
+++ src/test/java/junit/Ass1of4.java
@@ -0,0 +1,13 @@
+package junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Ass1of4 {
+
+@Test
+    public void test01(){
+    Assertions.assertTrue(true,"");
+}
+
+}
